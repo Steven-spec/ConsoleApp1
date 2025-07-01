6972b1f7fff65c434bc818b88f1d53cd41d169c6
Steven-spec <reynasteven006@gmail.com>
2025-07-01 12:40:14 -0500

    agregar

---
+++ ConsoleApp1/Tienda.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Tienda
+    {
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public List<Sucursal> Sucursales { get; set; }
+        public Tienda(string nombre, string direccion, string telefono)
+        {
+            Nombre = nombre;
+            Direccion = direccion;
+            Telefono = telefono;
+            Sucursales = new List<Sucursal>();
+        }
+        public void AgregarSucursal(Sucursal sucursal)
+        {
+            Sucursales.Add(sucursal);
+        }   
+    }
+}
